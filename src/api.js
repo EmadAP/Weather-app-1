2d9edffbb3240a8dddf730cf8db41df5b579198d
Emad AP <emad.amoupour1997@gmail.com>
2024-04-11 02:25:16 +0330

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "cc652e70bemsh4612bde1008d785p1ef34ajsn3bd24a90ed32",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_API_KEY = "baa9147db7a5391a8d652aa6d19090b6";
